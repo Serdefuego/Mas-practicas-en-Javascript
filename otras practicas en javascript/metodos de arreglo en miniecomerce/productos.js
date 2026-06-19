9754921c1444d4aa180fee3daf87d79eb4adc96d
unknown <mnlmutan@gmail.com>
2026-06-19 09:55:48 -0300

    metiendole practica

---
+++ otras practicas en javascript/metodos de arreglo en miniecomerce/productos.js
@@ -0,0 +1,18 @@
+const productos = [
+            { imagen: "🍌", nombre: "Bananas", precio: 220 },
+            { imagen: "🍎", nombre: "Manzanas", precio: 200 },
+            { imagen: "🥝", nombre: "Kiwis", precio: 280 },
+            { imagen: "🍈", nombre: "Melón", precio: 350 },
+            { imagen: "🍍", nombre: "Piñas", precio: 320 },
+            { imagen: "🍅", nombre: "Tomates", precio: 140 },
+            { imagen: "🥥", nombre: "Cocos", precio: 270 },
+            { imagen: "🍉", nombre: "Sandías", precio: 200 },
+            { imagen: "🍑", nombre: "Duraznos", precio: 310 },
+            { imagen: "🫐", nombre: "Arándanos", precio: 650 },
+            { imagen: "🥭", nombre: "Mango", precio: 290 },
+            { imagen: "🍇", nombre: "Uvas", precio: 700 },
+            { imagen: "🍐", nombre: "Peras", precio: 320 },
+            { imagen: "🍒", nombre: "Cerezas", precio: 1100 },
+            { imagen: "🍓", nombre: "Frutillas", precio: 600 },
+            { imagen: "🍋", nombre: "Limones", precio: 260 }
+        ];
